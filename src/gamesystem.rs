409d4c9a996c0da08ea3bdd414355da200c9b43a
Chris Prather <chris@prather.org>
2021-02-01 18:47:35 -0500

    WIP: get WoD HP to work

--- src/gamesystem.rs
+++ src/gamesystem.rs
@@ -1,4 +1,5 @@
 use super::{Skill, Skills};
+use itertools::Itertools;
 
 pub fn roll_stat() -> i32 {
     let roll = crate::rng::roll_dice(1, 6);
@@ -12,32 +13,29 @@ pub fn roll_stat() -> i32 {
     val
 }
 
-pub fn attr_bonus(value: i32) -> i32 {
-    0
+pub fn player_hp_at_level(con: i32, level: i32) -> i32 {
+    let n: i32 = 1 + con + (level / 2);
+    let hp = (0..n)
+        .map(|_| crate::rng::roll_dice(1, 6))
+        .sorted()
+        .rev()
+        .take(level as usize)
+        .sum();
+    hp
 }
 
-pub fn player_hp_per_level(fitness: i32) -> i32 {
-    15 + attr_bonus(fitness)
-}
-
-pub fn player_hp_at_level(fitness: i32, level: i32) -> i32 {
-    15 + (player_hp_per_level(fitness) * level)
-}
-
-pub fn npc_hp(fitness: i32, level: i32) -> i32 {
-    let mut total = 1;
-    for _i in 0..level {
-        total += i32::max(1, 8 + attr_bonus(fitness));
-    }
-    total
-}
-
-pub fn mana_per_level(intelligence: i32) -> i32 {
-    i32::max(1, 4 + attr_bonus(intelligence))
+pub fn npc_hp(con: i32, level: i32) -> i32 {
+    let n: i32 = 1 + con + (level / 2);
+    let hp = (0..n)
+        .map(|_| crate::rng::roll_dice(1, 6))
+        .sorted()
+        .take(level as usize)
+        .sum();
+    hp
 }
 
 pub fn mana_at_level(intelligence: i32, level: i32) -> i32 {
-    mana_per_level(intelligence) * level
+    4 * level
 }
 
 pub fn skill_bonus(skill: Skill, skills: &Skills) -> i32 {
